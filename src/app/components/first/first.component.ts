16c6ba5a1fdefce1f990ea787a8e0193b645499f
aymen2782 <aymen.sellaouti@gmail.com>
2021-11-15 14:33:17 +0100

    binding uni dir

--- src/app/components/first/first.component.ts
+++ src/app/components/first/first.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent implements OnInit {
-
+  isHidden = false;
+  message = '';
   constructor() { }
 
+  // attributs : état classe
+  // méthodes: Fonctionnalités
   ngOnInit(): void {
+  }
+  alerter() {
+    alert('cc');
+  }
+  showHide() {
+    this.isHidden = ! this.isHidden;
+  }
+  changeMessage(msg: string) {
+    this.message = msg;
   }
 
 }
